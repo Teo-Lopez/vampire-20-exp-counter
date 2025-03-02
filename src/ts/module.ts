f08083c7b84d771e1e892d9f3468fa5f77f60fbb
Teo Lopez <teo.lopez@cabify.com>
2025-03-02 22:31:07 +0100

    test hook

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,5 +1,6 @@
 // Do not remove this import. If you do Vite will think your styles are dead
 // code and not include them in the build output.
+import { DatabaseUpdateOperation } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/_types.mjs';
 import '../styles/style.scss';
 import DogBrowser from './apps/dogBrowser';
 import ExperienceCounter from './apps/experienceCounter';
@@ -22,4 +23,11 @@ Hooks.on('renderActorDirectory', (_: Application, html: JQuery) => {
     module.dogBrowser.render(true);
   });
   html.find('.directory-header .action-buttons').append(button);
+});
+
+Hooks.on('updateActor', (actor: Actor, changed, options: DatabaseUpdateOperation, userId: string) => {
+  console.log('actor', actor);
+  console.log('changed', changed);
+  console.log('options', options);
+  console.log('userId', userId);
 });
