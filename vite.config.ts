d1f9e5e238e551f7cba462eaba423628e74602b9
ambarino <emaildeteo@gmail.com>
2025-04-11 14:07:33 +0200

    add config for dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,73 +1,73 @@
-import * as fsPromises from "fs/promises";
-import copy from "rollup-plugin-copy";
-import scss from "rollup-plugin-scss";
-import { defineConfig, Plugin } from "vite";
+import * as fsPromises from 'fs/promises'
+import copy from 'rollup-plugin-copy'
+import scss from 'rollup-plugin-scss'
+import { defineConfig, Plugin } from 'vite'
 
-const moduleVersion = process.env.MODULE_VERSION;
-const githubProject = process.env.GH_PROJECT;
-const githubTag = process.env.GH_TAG;
+const moduleVersion = process.env.MODULE_VERSION
+const githubProject = process.env.GH_PROJECT
+const githubTag = process.env.GH_TAG
 
-console.log(process.env.VSCODE_INJECTION);
-
+console.log(process.env.VSCODE_INJECTION)
+const distPath = import.meta.env.DEV ? '../vampire-20-exp-counter' : 'dist'
 export default defineConfig({
   build: {
     sourcemap: true,
     rollupOptions: {
-      input: "src/ts/module.ts",
+      input: 'src/ts/module.ts',
       output: {
-        dir: "dist/scripts",
-        entryFileNames: "module.js",
-        format: "es",
+        dir: `${distPath}/scripts`,
+        entryFileNames: 'module.js',
+        format: 'es',
       },
     },
   },
   plugins: [
     updateModuleManifestPlugin(),
     scss({
-      output: "dist/style.css",
+      output: `${distPath}/style.css`,
       sourceMap: true,
-      watch: ["src/styles/*.scss"],
+      watch: ['src/styles/*.scss'],
     }),
     copy({
       targets: [
-        { src: "src/languages", dest: "dist" },
-        { src: "src/templates", dest: "dist" },
+        { src: 'src/languages', dest: `${distPath}` },
+        { src: 'src/templates', dest: `${distPath}` },
       ],
-      hook: "writeBundle",
+      hook: 'writeBundle',
     }),
   ],
-});
+})
 
 function updateModuleManifestPlugin(): Plugin {
   return {
-    name: "update-module-manifest",
+    name: 'update-module-manifest',
     async writeBundle(): Promise<void> {
       const packageContents = JSON.parse(
-        await fsPromises.readFile("./package.json", "utf-8")
-      ) as Record<string, unknown>;
-      const version = moduleVersion || (packageContents.version as string);
+        await fsPromises.readFile('./package.json', 'utf-8')
+      ) as Record<string, unknown>
+      const version = moduleVersion || (packageContents.version as string)
       const manifestContents: string = await fsPromises.readFile(
-        "src/module.json",
-        "utf-8"
-      );
+        'src/module.json',
+        'utf-8'
+      )
       const manifestJson = JSON.parse(manifestContents) as Record<
         string,
         unknown
-      >;
-      manifestJson["version"] = version;
+      >
+      manifestJson['version'] = version
       if (githubProject) {
-        const baseUrl = `https://github.com/${githubProject}/releases`;
-        manifestJson["manifest"] = `${baseUrl}/latest/download/module.json`;
+        const baseUrl = `https://github.com/${githubProject}/releases`
+        manifestJson['manifest'] = `${baseUrl}/latest/download/module.json`
         if (githubTag) {
           manifestJson[
-            "download"
-          ] = `${baseUrl}/download/${githubTag}/module.zip`;
+            'download'
+          ] = `${baseUrl}/download/${githubTag}/module.zip`
         }
       }
       await fsPromises.writeFile(
-        "dist/module.json",
+        `${distPath}/module.json',
         JSON.stringify(manifestJson, null, 4)
-      );
+      )
     },
-  };
+  }
 }
